2e67ec792efc5e251ceabd2515333303397f7262
Abel Hagos <hagosabel000@gmail.com>
2026-07-08 13:08:32 +0300

    docs: explain product controller CRUD methods

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const db = require('../models/db');
 
+//crud operations for products
 exports.getAllProducts = (req, res) => {
     db.query('SELECT * FROM products', (err, results) => {
         if (err) return res.status(500).send('Error fetching products');
